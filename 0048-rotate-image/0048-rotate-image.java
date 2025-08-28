afcc2f04e7b5f5c61b1a5dbfae00441c0164d8af
Ashutosh Mishra <122792997+ashutoshmishra4208@users.noreply.github.com>
2025-08-28 08:45:07 +0530

    Time: 0 ms (100%), Space: 42.2 MB (59.19%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -1,22 +1,41 @@
-class Solution {
-    public void rotate(int[][] matrix) {
+// class Solution {
+//     public void rotate(int[][] matrix) {
+//         int n = matrix.length;
+
+
+//         for (int i = 0; i < n; i++) {
+//             for (int j = i + 1; j < n; j++) {
+//                 int temp = matrix[i][j];
+//                 matrix[i][j] = matrix[j][i];
+//                 matrix[j][i] = temp;
+//             }
+//         }
+
+
+//         for (int i = 0; i < n; i++) {
+//             for (int j = 0; j < n / 2; j++) {
+//                 int temp = matrix[i][j];
+//                 matrix[i][j] = matrix[i][n - 1 - j];
+//                 matrix[i][n - 1 - j] = temp;
+//             }
+//         }
+//     }
+// }
+
+
+class Solution{
+    public void rotate(int[][] matrix){
         int n = matrix.length;
+        int matrix2[][] = new int[n][n];
+        for(int j = 0; j<n ; j++){
+            for(int i = n-1; i>=0; i--){
+                matrix2[j][n-i-1] = matrix[i][j];
 
-
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                int temp = matrix[i][j];
-                matrix[i][j] = matrix[j][i];
-                matrix[j][i] = temp;
             }
         }
-
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n / 2; j++) {
-                int temp = matrix[i][j];
-                matrix[i][j] = matrix[i][n - 1 - j];
-                matrix[i][n - 1 - j] = temp;
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                matrix[i][j] = matrix2[i][j];
             }
         }
     }
